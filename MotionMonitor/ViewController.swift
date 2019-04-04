777ec4603fa53f04115f3d5910c24127887eae0d
harshkeshwala <keshwalaharsh@gmail.com>
2019-04-04 19:31:04 -0400

    Initial Commit

---
+++ MotionMonitor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MotionMonitor
+//
+//  Created by Harsh Keshwala on 2019-04-04.
+//  Copyright © 2019 CentennialCollege. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
