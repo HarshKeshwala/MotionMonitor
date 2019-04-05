faf2eb46bc00ed149d6abb7d53e334d12139b923
harshkeshwala <keshwalaharsh@gmail.com>
2019-04-05 16:53:56 -0400

    Step Counter Added

--- MotionMonitor/ViewController.swift
+++ MotionMonitor/ViewController.swift
@@ -13,11 +13,17 @@ class ViewController: UIViewController {
 
     @IBOutlet var gyroscopeLabel: UILabel!
     @IBOutlet var accelerometerLabel: UILabel!
-    @IBOutlet var attitudeLabel: UILabel!
+    
     
     private let motionManager = CMMotionManager()
     private let queue = OperationQueue()
     
+    private var counter = 0
+    
+    @IBAction func resetButton(_ sender: UIButton) {
+        self.counter = 0
+        self.accelerometerLabel.text = String("Steps Taken:\n\n\(self.counter)")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -26,34 +32,18 @@ class ViewController: UIViewController {
             motionManager.startDeviceMotionUpdates(to: queue) {
                 (CMDeviceMotion, NSError) in
                 if let motion = CMDeviceMotion {
-                    let rotationRate = motion.rotationRate
-                    let gravity = motion.gravity
                     let userAcc = motion.userAcceleration
-                    let attitude = motion.attitude
-                    
-                    let gyroscopeText =
-                        String(format: "Rotation Rate:\n----------\n" +
-                            "x: %+.2f\ny: %+.2f\nz: %+.2f\n",
-                               rotationRate.x,rotationRate.y, rotationRate.z)
-                    
-                    let acceleratorText =
-                        String(format: "Acceleration: \n----------\n" +
-                            "Gravity x: %+.2f\t\tUser x: %.2f\n" +
-                            "Gravity y: %+.2f\t\tUser y: %.2f\n" +
-                            "Gravity z: %+.2f\t\tUser z: %.2f\n",
-                               gravity.x, userAcc.x, gravity.y,
-                               userAcc.y, gravity.z, userAcc.z)
-                    
-                    let attitudeText =
-                        String(format: "Attitude:\n--------\n" +
-                            "Roll: %+.2f\nPitch: %+.2f\nYaw:%+.2f\n",
-                               attitude.roll, attitude.pitch, attitude.yaw)
+
+                    if ((userAcc.x > 1) || (userAcc.y > 1) || (userAcc.z > 1)) {
+                        print("Step Taken")
+                        self.counter = self.counter + 1
+                    }
                     
                     DispatchQueue.main.async {
-                        self.gyroscopeLabel.text = gyroscopeText
-                        self.accelerometerLabel.text = acceleratorText
-                        self.attitudeLabel.text = attitudeText
+                        self.gyroscopeLabel.text = "Step Counter"
+                        self.accelerometerLabel.text = String("Steps Taken:\n\n\(self.counter)")
                     }
+                    
                     
                 }
             }
